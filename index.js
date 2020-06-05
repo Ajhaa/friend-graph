aa604d41ea146aeb39d32f541084bc02b0239839
atte <atte.h.1998@gmail.com>
2020-06-05 17:14:22 +0300

    Improvements

--- index.js
+++ index.js
@@ -3,43 +3,32 @@ const express = require('express')
 
 const app = express()
 
-
-app.get('/', (req, res) => {
-  faker.seed()
-
-  let people = []
-
-  for (let i = 0; i < 100; i++) {
-    people.push({ fn: faker.name.firstName(), ln: faker.name.lastName() })
-  }
-
-  return res.json(people)
-})
-
 app.get('/:seed', (req, res) => {
   const { seed } = req.params
+
   if (Number.isNaN(Number(seed))) {
     return res.status(400).send('seed needs to be a number')
   }
+  
   faker.seed(Number(seed))
 
   let people = []
 
   for (let i = 0; i < 100; i++) {
-    people.push({ uuid: faker.random.uuid(), fn: faker.name.firstName(), ln: faker.name.lastName(), friends: [] })
+    people.push({ uuid: faker.random.uuid(), fn: faker.name.firstName(), ln: faker.name.lastName(), friends: new Set() })
   }
 
   for (person of people) {
     for (friend of people) {
       if (faker.random.number() % 50 <= 2) {
-        person.friends.push(friend.uuid)
-        friend.friends.push(person.uuid)
+        person.friends.add(friend.uuid)
+        friend.friends.add(person.uuid)
       }
     }
   }
 
-  return res.json(people)
+  return res.json(people.map(p => ({ ...p, friends: Array.from(p.friends)})))
 })
 
 
-app.listen(3000, () => console.log('l'))
+app.listen(3000, () => console.log('listening in 3000'))
